ce331ed5d75b2e229cf05afcac61e1bcd2bf3a10
bogdan <bogdan.boboc@capgemini.com>
2023-04-06 11:30:23 +0300

    Sovles test

---
+++ ExcelSheetColumnLetterToColumnNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Please insert the column you wish to get the number of:");
+        var columnLetters = Console.ReadLine();
+        Console.WriteLine("Would you like to use lowercase letters aswell?(Y=Yes/N=No default N)");
+        var allowSmall = (Console.ReadKey().KeyChar=='y')?true:false;
+
+        try
+        {
+            Console.WriteLine($"\nThe number of the desired column is {GetExcelColumnNumber(columnLetters, allowSmall)}");
+        } catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Exception: {ex.Message}");
+        }
+    }
+
+    public static int GetExcelColumnNumber(string columnLetter, bool allowSmallLetters = false)
+    {
+        if(allowSmallLetters)
+        {
+            columnLetter = columnLetter.ToUpper();
+        }
+        var letter = columnLetter.AsEnumerable<char>().ToArray();
+        int column = 0;
+        for (int i=0;i< letter.Length; i++)
+        {
+            if (letter[i] < 'A' || letter[i] > 'Z') {
+                throw new ArgumentException($"The character format {letter[i]} is not inside the upper case alphabet." +
+                    $"\n Please use only english uppercase letters.");
+            }
+            column = column*26 + (letter[i] - 'A' + 1);
+        }
+        return column;
+    }
+
+}
---
+++ ExcelSheetTest/UnitTest1.cs
@@ -0,0 +1,62 @@
+namespace ExcelSheetTest
+{
+    using Xunit;
+
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("A",1)]
+        [InlineData("B", 2)]
+        [InlineData("C", 3)]
+        [InlineData("D", 4)]
+        [InlineData("E", 5)]
+        [InlineData("X", 24)]
+        [InlineData("Y", 25)]
+        [InlineData("Z", 26)]
+        public void TestForSingleLetter_ValueIsPredefined_ShouleBeEqual(string letter,int number)
+        {
+            // Arrange
+
+            // Act
+            var column = Program.GetExcelColumnNumber(letter);
+
+            // Assert
+            Assert.Equal(number, column);
+        }
+
+        [Theory]
+        [InlineData("AA", 27)]
+        [InlineData("BBC", 1407)]
+        [InlineData("YZ", 676)]
+        [InlineData("ZZ", 702)]
+        [InlineData("AAC", 705)]
+        public void TestForMultipleLetters_ValueIsPredefined_ShouleBeEqual(string letter, int number)
+        {
+            // Arrange
+
+            // Act
+            var column = Program.GetExcelColumnNumber(letter);
+
+            // Assert
+            Assert.Equal(number, column);
+        }
+
+        [Theory]
+        [InlineData("AA",false, false)]
+        [InlineData("Bb", true, false)]
+        [InlineData("Yz", false, true)]
+        [InlineData("BB ", true, true)]
+        [InlineData("BA3", true, true)]
+        [InlineData("BQ@", false, true)]
+        public void MultipleLetters_TestingForLowerCaseAndRandomChars_ShouldThrowErrorWhenCharacterIsNotAllowed(string letter,bool allowsmall, bool shouldThrowException)
+        {
+            // Arrange
+
+            // Act
+            var exception =Record.Exception(() => Program.GetExcelColumnNumber(letter,allowsmall));
+
+            // Assert
+            Assert.Equal(exception!=null, shouldThrowException);
+        }
+    }
+}
